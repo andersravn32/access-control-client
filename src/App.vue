72223d6b01706682df35b7bb3cdde0c9a9b4b939
Anders Ravn <anders@adriansen.dk>
2023-05-22 11:02:06 +0200

    Add: Added base app component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<script setup>
-import { onMounted } from "vue";
-import { storeToRefs } from "pinia";
-import useAuthStore from "./stores/authStore";
-
-const authStore = useAuthStore();
-</script>
-
 <template>
   <router-view />
 </template>
