2dd0c5f2f49c49f5e44ee3d7b747bea5f74fd65a
Anders Ravn <anders@adriansen.dk>
2023-05-22 11:01:56 +0200

    Add: Added socket.io

---
+++ src/socket.js
@@ -0,0 +1,12 @@
+import { ref } from "vue";
+import { io } from "socket.io-client";
+
+export const state = ref({
+  connected: false,
+});
+
+export const socket = io("http://127.0.0.1:4000", {
+  auth: {
+    token: localStorage.getItem("accessToken"),
+  },
+});
