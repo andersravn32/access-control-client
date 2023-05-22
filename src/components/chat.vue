aeb4955c03149814c7580168e0a116e2a8bb8b47
Anders Ravn <anders@adriansen.dk>
2023-05-22 11:02:21 +0200

    Add: Added chat component

---
+++ src/components/chat.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { socket } from '../socket';
+import { onMounted, onUnmounted } from "vue";
+
+onMounted(() => {
+    socket.emit("chat-join");
+})
+
+onUnmounted(() => {
+    socket.emit("chat-leave");
+})
+</script>
+
+<template>
+  <div class="chat">
+
+  </div>
+</template>
