e43fa17fbdef0361928de846394223d43392cba9
Anders Ravn <anders@adriansen.dk>
2023-05-22 11:03:14 +0200

    Add: Added chat page

--- src/pages/chat.vue
+++ src/pages/chat.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { socket } from "../socket";
+import Chat from "../components/chat.vue";
+</script>
+
 <template>
-    
+  <section id="page-chat">
+    <Chat  />
+  </section>
 </template>
